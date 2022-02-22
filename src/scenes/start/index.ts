96f427acbd1163f462dadc04f95315b025a8903b
bonkrat <ross.jernigan@gmail.com>
2022-02-22 14:59:25 -0500

    chore: cleanup intro scene

--- src/scenes/start/index.ts
+++ src/scenes/start/index.ts
@@ -1,12 +1,10 @@
 import { getRandomEnemyClass } from "../../sprites/enemies";
-import { Introduction } from "../introduction";
 
 export class Start extends Phaser.Scene {
   constructor() {
     super("Start");
   }
   create() {
-    // this.scene.start("Battle", { enemyClass: getRandomEnemyClass() });
     this.scene.start("Introduction", { enemyClass: getRandomEnemyClass() });
   }
 }
