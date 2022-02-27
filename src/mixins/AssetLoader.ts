4c85a3d85bf5bf55dd29cedee74410a561a40222
bonkrat <ross.jernigan@gmail.com>
2022-02-27 15:57:27 -0500

    chore: use separate promises for loading assets

--- src/mixins/AssetLoader.ts
+++ src/mixins/AssetLoader.ts
@@ -19,28 +19,7 @@ export function LoadableAssets<TBase extends Loadable>(
     }
 
     async loadAssets() {
-      const loaderPromise = new Promise<void>((resolve, reject) => {
-        this.scene.load.on(
-          "complete",
-          (
-            _loader: typeof Phaser.Loader,
-            _complete: number,
-            failed: number
-          ) => {
-            if (failed) {
-              reject();
-            }
-            this.debug(
-              "LOAD ASSETS",
-              "Promise fulfilled for this many assets: ",
-              _complete
-            );
-
-            resolve();
-          }
-        );
-      });
-
+      const promises = [] as Promise<void>[];
       this.assets.forEach(async (asset) => {
         const spriteConfig = asset.spriteConfig;
         const key = spriteConfig?.key || this.key;
@@ -54,6 +33,7 @@ export function LoadableAssets<TBase extends Loadable>(
               key,
               ...asset?.spriteConfig,
             });
+            promises.push(this.fileCompletePromise(key));
           }
         } else {
           throw new Error("Missing key when loading assets for " + Base.name);
@@ -62,11 +42,6 @@ export function LoadableAssets<TBase extends Loadable>(
 
       this.debug("LOAD ASSETS", "STARTING...");
       this.scene.load.start();
-      this.debug(
-        "LOAD ASSETS",
-        "loader state",
-        this.scene.load.state ? "RUNNING" : "NOT RUNNING"
-      );
 
       if (this.scene.load.totalToLoad) {
         this.debug(
@@ -76,7 +51,7 @@ export function LoadableAssets<TBase extends Loadable>(
         );
       }
 
-      await loaderPromise;
+      await Promise.all(promises);
 
       this.debug("LOAD ASSETS", "FINISHED!");
 
@@ -111,15 +86,16 @@ export function LoadableAssets<TBase extends Loadable>(
 
           this.scene.anims.create(animation);
         }
+      });
+    }
 
-        // } else {
-        //   console.warn(
-        //     "Tried loading another animation for key " +
-        //       key +
-        //       " for asset " +
-        //       Base.name
-        //   );
-        // }
+    private fileCompletePromise(key: string) {
+      return new Promise<void>((resolve) => {
+        this.scene.load.on("filecomplete-spritesheet-" + key, () => {
+          this.debug("LOAD ASSETS", "loading asset " + key + " complete.");
+
+          resolve();
+        });
       });
     }
 
